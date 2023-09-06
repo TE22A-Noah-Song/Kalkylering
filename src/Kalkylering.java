e3ac29ba990d71699eaf437fcf9539649360ee26
Noah Söng <noah.song@elev.ga.ntig.se>
2023-09-06 10:58:31 +0200

    beräknar, skriver ut alla 4 operatörer med decimal

--- src/Kalkylering.java
+++ src/Kalkylering.java
@@ -2,18 +2,20 @@ public class Kalkylering {
     public static void main(String[] args) throws Exception {
         int tal1=23;
         int tal2=45;
+        double tal3=15;
+        double tal4=4;
         int summa;
         int differens;
         int produkt;
-        int kvot;
+        double kvot;
         summa=tal1+tal2;
         differens=tal1-tal2;
         produkt=tal1*tal2;
-        kvot=tal1/tal2;
+        kvot=tal3/tal4;
         System.out.println(tal1+"+"+tal2+"="+summa);
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println(tal1+"-"+tal2+"="+differens);
+        System.out.println(tal1+"*"+tal2+"="+produkt);
+        System.out.println(tal3+"/"+tal4+"="+kvot);
 
 
     }
