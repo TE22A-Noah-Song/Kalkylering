ae26360ea14f0a859c233d233a5335371824fd28
Noah Söng <noah.song@elev.ga.ntig.se>
2023-09-06 10:51:15 +0200

    Lagt till alla variabler (2tal 4operatörer)

---
+++ src/Kalkylering.java
@@ -0,0 +1,20 @@
+public class Kalkylering {
+    public static void main(String[] args) throws Exception {
+        int tal1=23;
+        int tal2=45;
+        int summa;
+        int differens;
+        int produkt;
+        int kvot;
+        summa=tal1+tal2;
+        differens=tal1-tal2;
+        produkt=tal1*tal2;
+        kvot=tal1/tal2;
+        System.out.println(tal1+"+"+tal2+"="+summa);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+    }
+}
